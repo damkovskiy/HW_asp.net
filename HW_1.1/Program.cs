c091301cf9fde73664120ae1b0d30ed0651b052c
Дмитрий Дамковский <damkovski@gmail.com>
2021-07-18 19:37:37 +0300

    added hw_1.1

---
+++ HW_1.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.Json;
+
+namespace HW_1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Song song = new Song();
+            song.CreateSong();
+
+            string json = JsonSerializer.Serialize(song);
+
+            Console.WriteLine("");
+            Console.WriteLine(json);
+        }
+    }
+}
---
+++ HW_1.1/Song.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace HW_1._1
+{
+    class Song
+    {
+        public string Name { get ; set ; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+
+        public void CreateSong()
+            {
+                Console.WriteLine("Input name of song");
+                Name = Console.ReadLine();
+                Console.WriteLine("Input author of song");
+                Author = Console.ReadLine();
+                Console.WriteLine("Input year of song");
+                Year = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
